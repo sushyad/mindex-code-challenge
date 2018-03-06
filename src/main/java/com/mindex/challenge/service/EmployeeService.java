cdfc0362eba2f77c577245097f40c1142dc9c073
Matthew Mason <mmason3@paychex.com>
2018-03-06 14:29:36 -0500

    Test case and README

--- src/main/java/com/mindex/challenge/service/EmployeeService.java
+++ src/main/java/com/mindex/challenge/service/EmployeeService.java
@@ -5,5 +5,5 @@ import com.mindex.challenge.data.Employee;
 public interface EmployeeService {
     Employee create(Employee employee);
     Employee read(String id);
-    Employee update(String id, Employee employee);
+    Employee update(Employee employee);
 }
